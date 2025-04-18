1242cdbff4f165cf40aae0df31492e36f2b8ebaf
dfelix-12 <dfelix12@terpmail.umd.edu>
2025-04-18 10:36:36 -0400

    seq_function
    This is the start of the interim deliverable. Add your functions for the assignment due tonight

---
+++ seq_func.py
@@ -0,0 +1,9 @@
+import random
+
+# function for updating length of sequence and corrupted sequence
+
+
+# Function will cause an error because it hasn't been initialized
+length = 3 + level
+sequence = [str(random.randint(0,9))for _ in range(length)]
+    return sequence
